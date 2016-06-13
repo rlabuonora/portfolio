24d610f53628c40866ab429430d47c3c626d3534
Rafael La Buonora <rafaretrogrado@gmail.com>
2016-06-13 19:20:42 +0000

    Agregar pagina proyectos

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ var express = require('express');
 var app = express();
 
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
 app.use(express.static('public'));
 app.use(express.static('node_modules/bootstrap/dist'));
+
+app.get('/proyectos', function(request, response) {
+  response.render('pages/index');
+})
 
 
 module.exports = app;
